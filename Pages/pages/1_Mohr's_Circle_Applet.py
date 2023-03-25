3183df38de1c5f98fe4deec401ffd9873fca9be2
alanifamily <78260805+alanifamily@users.noreply.github.com>
2023-03-25 16:15:06 +0300

    Update 1_Mohr's_Circle_Applet.py

--- Pages/pages/1_Mohr's_Circle_Applet.py
+++ Pages/pages/1_Mohr's_Circle_Applet.py
@@ -54,7 +54,7 @@ fig.update_xaxes(nticks=5)
 fig.update_yaxes(nticks=5,scaleanchor = "x",scaleratio = 1,)
 
 
-fig.update_layout(xaxis_title="σ"+Unit, yaxis_title='test '+Unit)
+fig.update_layout(xaxis_title="σ "+Unit, yaxis_title='τ '+Unit)
 fig.update_yaxes(autorange="reversed")
 
 
